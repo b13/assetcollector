b9e87143ec806a9f77d136c46ace1df1748de6ed
Achim Fritz <achim.fritz@b13.de>
2019-09-03 09:41:51 +0200

    [BUGFIX] change middlewares ordering
    Relates to: BWEB-493

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -3,11 +3,8 @@ return [
     'frontend' => [
         'b13/assetcollector' => [
             'target' => \B13\Assetcollector\Middleware\AssetRenderer::class,
-            'before' => [
-                'typo3/cms-frontend/content-length-headers'
-            ],
             'after' => [
-                'typo3/cms-frontend/prepare-tsfe-rendering',
+                'typo3/cms-frontend/content-length-headers'
             ]
         ]
     ]
