73f682b1ea4388a21087076ed91897ee86929d90
Achim Fritz <af@achimfritz.de>
2025-11-19 14:29:48 +0100

    [BUGFIX] do not save absPath of xmlFiles
    absolute File Paths can differ when TYPO3 is installed
    on different nodes

---
+++ Tests/Functional/AssetCollectorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace B13\Assetcollector\Tests\Functional;
+
+/*
+ * This file is part of TYPO3 CMS-based extension "assetcollector" by b13.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+use B13\Assetcollector\AssetCollector;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class AssetCollectorTest extends FunctionalTestCase
+{
+    protected array $testExtensionsToLoad = ['typo3conf/ext/assetcollector'];
+    protected array $coreExtensionsToLoad = ['core'];
+
+    /**
+     * @test
+     */
+    public function buildXmlTagBuildsXmlTagForExistingFile(): void
+    {
+        $assetCollector = new AssetCollector();
+        $assetCollector->addXmlFile('EXT:assetcollector/Resources/Public/Icons/Extension.svg');
+        $content = $assetCollector->buildInlineXmlTag();
+        self::assertStringContainsString('<title>EXT:assetcollector</title', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function buildXmlTagIsEmptyForNoneExistingFile(): void
+    {
+        $assetCollector = new AssetCollector();
+        $assetCollector->addXmlFile('foo/not-exists.svg');
+        $content = $assetCollector->buildInlineXmlTag();
+        self::assertSame('', $content);
+    }
+}
